03c20de83d317b35010dfb351abddff2ad4e1a14
Shashank Pandey <jpshashank200@gmail.com>
2025-02-19 13:32:03 +0530

    Backend

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017")
+db = client['portfolio']
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/contactus', methods=['POST'])
+def contactus():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+
+    doc = {
+        'name' : name,
+        'email' : email,
+        'message' : message
+    }
+    
+    db.contactus.insert_one(doc)
+    return "Thank you for contacting us!"
+
+app.run(port=80)
